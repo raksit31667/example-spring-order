983f170255b4e3df302a6390b516eca2fc458fe6
raksit31667 <rak-sit@hotmail.com>
2019-02-05 01:10:33 +0700

    :heavy_plus_sign: Added Faker for random tests

--- src/main/java/com/raksit/example/order/common/util/MockOrderFactory.java
+++ src/main/java/com/raksit/example/order/common/util/MockOrderFactory.java
@@ -1,19 +1,25 @@
 package com.raksit.example.order.common.util;
 
+import com.github.javafaker.Faker;
 import com.raksit.example.order.common.model.entity.Order;
 import com.raksit.example.order.common.model.entity.OrderLineItem;
 import java.util.Collections;
 
 public class MockOrderFactory {
 
+  private static final Faker FAKER = new Faker();
+
   private static OrderLineItem createSampleOrderLineItems() {
-    return OrderLineItem.builder().name("Lube").price(2000.0).build();
+    return OrderLineItem.builder()
+        .name(FAKER.beer().name())
+        .price(FAKER.number().randomDouble(2, 1000, 2000))
+        .build();
   }
 
   public static Order createSampleOrder() {
     return Order.builder()
-        .source("Radiant")
-        .destination("Dire")
+        .source(FAKER.company().name())
+        .destination(FAKER.company().name())
         .items(Collections.nCopies(3, createSampleOrderLineItems()))
         .build();
   }
