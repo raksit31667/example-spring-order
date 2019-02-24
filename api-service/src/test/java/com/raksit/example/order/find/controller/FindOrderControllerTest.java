352ab7993854b3fbdf762268c24877f25e1ab9ac
raksit31667 <rak-sit@hotmail.com>
2019-02-24 14:04:36 +0700

    :white_check_mark: Added find order controller unit + integration tests

---
+++ api-service/src/test/java/com/raksit/example/order/find/controller/FindOrderControllerTest.java
@@ -0,0 +1,46 @@
+package com.raksit.example.order.find.controller;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.raksit.example.order.common.model.entity.Order;
+import com.raksit.example.order.find.service.FindOrderService;
+import com.raksit.example.order.util.MockOrderFactory;
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = FindOrderController.class, secure = false)
+public class FindOrderControllerTest {
+
+  private static final int NUMBER_OF_ITEMS = 3;
+
+  @Autowired
+  private MockMvc mvc;
+
+  @MockBean
+  private FindOrderService findOrderService;
+
+  @Test
+  public void getOrdersBySource_ShouldReturnOrdersWithSpecificSource() throws Exception {
+    Order thaiOrder = MockOrderFactory.createSampleOrder(NUMBER_OF_ITEMS);
+
+    when(findOrderService.getOrdersBySource(eq("Bangkok")))
+        .thenReturn(Collections.singletonList(thaiOrder));
+
+    mvc.perform(get("/orders")
+    .param("source", "Bangkok"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].source", is("Bangkok")));
+  }
+}
---
+++ api-service/src/test/java/com/raksit/example/order/find/controller/integration/FIndOrderControllerIntegrationTest.java
@@ -0,0 +1,63 @@
+package com.raksit.example.order.find.controller.integration;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.raksit.example.order.common.model.entity.Order;
+import com.raksit.example.order.common.repository.OrderRepository;
+import com.raksit.example.order.util.MockOrderFactory;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class FIndOrderControllerIntegrationTest {
+
+  private static final int NUMBER_OF_ITEMS = 3;
+
+  @Autowired private TestRestTemplate restTemplate;
+
+  @Autowired private OrderRepository orderRepository;
+
+  @AfterEach
+  public void tearDown() {
+    orderRepository.deleteAll();
+  }
+
+  @Test
+  public void getOrdersBySource_ShouldReturnOrdersWithSpecificSource() throws Exception {
+    Order thaiOrder = MockOrderFactory.createSampleOrder(NUMBER_OF_ITEMS);
+    Order chineseOrder = MockOrderFactory.createSampleOrder(NUMBER_OF_ITEMS);
+    thaiOrder.setSource("Bangkok");
+    chineseOrder.setSource("Wuhan");
+
+    orderRepository.save(thaiOrder);
+    orderRepository.save(chineseOrder);
+
+    UriComponentsBuilder uriBuilder =
+        UriComponentsBuilder.fromHttpUrl("/").queryParam("source", "Bangkok");
+
+    ResponseEntity<List<Order>> responseEntity =
+        restTemplate.getForEntity(
+            uriBuilder.toUriString(), null, new ParameterizedTypeReference<List<Order>>() {});
+
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertThat(responseEntity.getBody(), hasSize(1));
+    assertThat(responseEntity.getBody(), everyItem(hasProperty("source", is("Bangkok"))));
+  }
+}
