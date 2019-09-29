fd2331017078bb9b582ac2d4ecf3bcb1dd69da98
Raksit Mantanacharu <raksit.m@ku.th>
2019-09-29 19:07:04 +0700

    :recycle: REFACTOR. used stream and call map each item instead of bulk mapping.

--- api-service/src/main/java/com/raksit/example/order/find/service/implementation/DefaultFindOrderService.java
+++ api-service/src/main/java/com/raksit/example/order/find/service/implementation/DefaultFindOrderService.java
@@ -2,21 +2,27 @@ package com.raksit.example.order.find.service.implementation;
 
 import com.raksit.example.order.common.exception.OrderNotFoundException;
 import com.raksit.example.order.common.model.dto.OrderResponse;
+import com.raksit.example.order.common.model.entity.Order;
 import com.raksit.example.order.common.model.mapper.OrderMapper;
 import com.raksit.example.order.common.repository.OrderRepository;
 import com.raksit.example.order.find.service.FindOrderService;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DefaultFindOrderService implements FindOrderService {
 
-  @Autowired private OrderRepository orderRepository;
+  @Autowired
+  private OrderRepository orderRepository;
 
   @Override
   public List<OrderResponse> getOrdersBySource(String source) throws OrderNotFoundException {
-    return OrderMapper.INSTANCE.ordersToOrderResponses(
-        orderRepository.findAllBySource(source).orElseThrow(OrderNotFoundException::new));
+    List<Order> orders = orderRepository.findAllBySource(source)
+        .orElseThrow(OrderNotFoundException::new);
+
+    return orders.stream().map(OrderMapper.INSTANCE::orderToOrderResponse)
+        .collect(Collectors.toList());
   }
 }
