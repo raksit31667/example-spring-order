4fddf48067ee86b191c2b4faf606b18ef10f36c2
Raksit Mantanacharu <raksit.m@ku.th>
2020-01-07 20:18:11 +0700

    :truck: renamed Config suffix to Configuration

--- api-service/src/main/java/com/raksit/example/order/config/SecurityConfiguration.java
+++ api-service/src/main/java/com/raksit/example/order/config/SecurityConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity security) throws Exception {
