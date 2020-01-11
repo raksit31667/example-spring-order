a1da643361d55d888e4cbe6e297ac1c214ff0f7a
Raksit Mantanacharu <raksit.m@ku.th>
2020-01-11 16:15:55 +0700

    :pencil: added @ApiResponse status 404 for findOrderById when there is no order found in Swagger API

--- api-service/src/main/java/com/raksit/example/order/find/controller/FindOrderController.java
+++ api-service/src/main/java/com/raksit/example/order/find/controller/FindOrderController.java
@@ -1,29 +1,42 @@
 package com.raksit.example.order.find.controller;
 
+import com.raksit.example.order.common.exception.OrderExceptionResponse;
 import com.raksit.example.order.common.model.dto.OrderResponse;
 import com.raksit.example.order.find.service.FindOrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/orders")
 @Api(tags = {"Order"})
 public class FindOrderController {
 
   @Autowired private FindOrderService findOrderService;
 
-  @GetMapping("/orders")
+  @GetMapping
   @ApiOperation(value = "Get order by source (sold-to)")
   public List<OrderResponse> findOrdersBySource(@RequestParam String source) {
     return findOrderService.findOrdersBySource(source);
   }
 
-
-  public OrderResponse findOrderById(Long id) {
-    return findOrderService.findOrderById(id);
+  @GetMapping("/orderId")
+  @ApiResponses(
+      value = {
+          @ApiResponse(
+              code = 404,
+              message = "Order Not Found",
+              response = OrderExceptionResponse.class)
+      })
+  public OrderResponse findOrderById(@PathVariable Long orderId) {
+    return findOrderService.findOrderById(orderId);
   }
 }
