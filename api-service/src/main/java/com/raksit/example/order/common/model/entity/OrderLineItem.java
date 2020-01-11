7fa918777c738d9b6aaa04be638e756680dc466d
Raksit Mantanacharu <raksit.m@ku.th>
2020-01-11 21:31:21 +0700

    :recycle: removed unused imports from OrderLineItem.java

--- api-service/src/main/java/com/raksit/example/order/common/model/entity/OrderLineItem.java
+++ api-service/src/main/java/com/raksit/example/order/common/model/entity/OrderLineItem.java
@@ -1,12 +1,9 @@
 package com.raksit.example.order.common.model.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
