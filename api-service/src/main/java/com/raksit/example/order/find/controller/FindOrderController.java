2587f07f89b0b4f11f646eb7b346f7b5b3055c98
Raksit Mantanacharu <raksit.m@ku.th>
2020-01-11 16:06:50 +0700

    :ok_hand: 1.) removed unused @ApiResponse due to requirement changes 2.) removed @ResponseStatus because it is not necessary with @RestController

--- api-service/src/main/java/com/raksit/example/order/find/controller/FindOrderController.java
+++ api-service/src/main/java/com/raksit/example/order/find/controller/FindOrderController.java
@@ -1,18 +1,13 @@
 package com.raksit.example.order.find.controller;
 
-import com.raksit.example.order.common.exception.OrderExceptionResponse;
 import com.raksit.example.order.common.model.dto.OrderResponse;
 import com.raksit.example.order.find.service.FindOrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,18 +17,11 @@ public class FindOrderController {
   @Autowired private FindOrderService findOrderService;
 
   @GetMapping("/orders")
-  @ResponseStatus(HttpStatus.OK)
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            code = 404,
-            message = "Order Not Found",
-            response = OrderExceptionResponse.class)
-      })
   @ApiOperation(value = "Get order by source (sold-to)")
   public List<OrderResponse> findOrdersBySource(@RequestParam String source) {
     return findOrderService.findOrdersBySource(source);
   }
+
 
   public OrderResponse findOrderById(Long id) {
     return findOrderService.findOrderById(id);
