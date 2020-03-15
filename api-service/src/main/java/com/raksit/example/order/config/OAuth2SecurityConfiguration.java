d70f92dbb26b8f43d4f331b3c8aea358e1680531
Raksit Mantanacharu <raksit.m@ku.th>
2020-03-15 11:21:44 +0700

    :lock: configured JWT resource server to protect resources with predefined token store in any method containing @PreAuthorize

---
+++ api-service/src/main/java/com/raksit/example/order/config/OAuth2SecurityConfiguration.java
@@ -0,0 +1,76 @@
+package com.raksit.example.order.config;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.IssuerClaimVerifier;
+import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@Configuration
+public class OAuth2SecurityConfiguration {
+
+  @Autowired
+  private RequestMappingHandlerMapping requestMappingHandlerMapping;
+
+  @Value("${security.oauth2.resource.issuer}")
+  private String issuer;
+
+  @Value("${security.oauth2.resource.id}")
+  private String resourceId;
+
+  @Value("${security.oauth2.resource.jwk.keySetUri}")
+  private String keySetUri;
+
+  @Bean
+  protected ResourceServerConfiguration resourceServerConfiguration() {
+    ResourceServerConfiguration resourceServerConfiguration = new ResourceServerConfiguration();
+    resourceServerConfiguration.setConfigurers(Collections.singletonList(new ResourceServerConfigurerAdapter() {
+
+      @Override
+      public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        TokenStore tokenStore = jwkTokenStore();
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore);
+        resources.resourceId(resourceId)
+            .tokenStore(tokenStore)
+            .tokenServices(defaultTokenServices);
+      }
+
+      @Override
+      public void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers()
+            .mvcMatchers(getRequestMappingPatterns())
+            .and()
+            .authorizeRequests()
+            .anyRequest().fullyAuthenticated();
+      }
+    }));
+
+    return resourceServerConfiguration;
+  }
+
+  private TokenStore jwkTokenStore() throws MalformedURLException {
+    IssuerClaimVerifier issuerClaimVerifier = new IssuerClaimVerifier(new URL(issuer));
+    return new JwkTokenStore(keySetUri, issuerClaimVerifier);
+  }
+
+  private String[] getRequestMappingPatterns() {
+    return requestMappingHandlerMapping.getHandlerMethods()
+        .entrySet().stream()
+        .filter(entry -> entry.getValue().getMethodAnnotation(PreAuthorize.class) != null)
+        .flatMap(entry -> entry.getKey().getPatternsCondition().getPatterns().stream())
+        .toArray(String[]::new);
+  }
+}
