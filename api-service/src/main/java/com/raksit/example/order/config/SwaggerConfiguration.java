92b444d6714000b3487936bddc821ff55aa7c88b
Raksit Mantanacharu <raksit.m@ku.th>
2020-03-31 23:12:31 +0700

    :lock: autocompleted azure resource id in swagger ui's available authorizations

--- api-service/src/main/java/com/raksit/example/order/config/SwaggerConfiguration.java
+++ api-service/src/main/java/com/raksit/example/order/config/SwaggerConfiguration.java
@@ -22,6 +22,8 @@ import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static com.google.common.collect.Sets.newHashSet;
@@ -50,6 +52,13 @@ public class SwaggerConfiguration {
         .useDefaultResponseMessages(false)
         .securityContexts(securityContexts())
         .securitySchemes(securitySchemes());
+  }
+
+  @Bean
+  public SecurityConfiguration swaggerSecurityConfiguration() {
+    return SecurityConfigurationBuilder.builder()
+        .clientId(resourceId)
+        .build();
   }
 
   private ApiInfo apiInfo() {
