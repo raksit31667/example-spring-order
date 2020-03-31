285e1bb1e473c92e67fb0fbd29dfb589364e6511
Raksit Mantanacharu <raksit.m@ku.th>
2020-03-31 22:12:30 +0700

    :pencil: updated findOrderById and findOrdersBySource swagger information

--- api-service/src/main/java/com/raksit/example/order/find/controller/FindOrderController.java
+++ api-service/src/main/java/com/raksit/example/order/find/controller/FindOrderController.java
@@ -5,6 +5,8 @@ import com.raksit.example.order.common.model.dto.OrderResponse;
 import com.raksit.example.order.find.service.FindOrderService;
 import com.raksit.example.order.find.validator.ValidOrderId;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -25,11 +27,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = {"Order"})
 public class FindOrderController {
 
-  @Autowired private FindOrderService findOrderService;
+  @Autowired
+  private FindOrderService findOrderService;
 
   @GetMapping
   @ApiOperation("Get order by source (sold-to)")
   @PreAuthorize("hasApplicationRole('READ')")
+  @ApiResponses({
+      @ApiResponse(code = 401, message = "Unauthorized"),
+      @ApiResponse(code = 403, message = "Forbidden"),
+      @ApiResponse(code = 500, message = "Internal Server Error")
+  })
+  @ApiImplicitParams({
+      @ApiImplicitParam(paramType = "query", name = "source",
+          dataType = "String", value = "order source")
+  })
   public List<OrderResponse> findOrdersBySource(@RequestParam String source) {
     return findOrderService.findOrdersBySource(source);
   }
@@ -37,13 +49,17 @@ public class FindOrderController {
   @GetMapping("/{orderId}")
   @ApiOperation("Get order by id")
   @PreAuthorize("hasApplicationRole('READ')")
-  @ApiResponses(
-      value = {
-          @ApiResponse(
-              code = 404,
-              message = "Order Not Found",
-              response = OrderExceptionResponse.class)
-      })
+  @ApiResponses({
+      @ApiResponse(code = 400, message = "Bad Request", response = OrderExceptionResponse.class),
+      @ApiResponse(code = 401, message = "Unauthorized"),
+      @ApiResponse(code = 403, message = "Forbidden"),
+      @ApiResponse(code = 404, message = "Not Found", response = OrderExceptionResponse.class),
+      @ApiResponse(code = 500, message = "Internal Server Error")
+  })
+  @ApiImplicitParams({
+      @ApiImplicitParam(paramType = "path", name = "orderId",
+          dataType = "String", value = "order id (UUID format)")
+  })
   public OrderResponse findOrderById(@PathVariable @ValidOrderId String orderId) {
     return findOrderService.findOrderById(UUID.fromString(orderId));
   }
